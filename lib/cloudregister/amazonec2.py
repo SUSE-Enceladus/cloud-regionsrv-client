7718e1ca0bcd67474802c1e289e4eb7d6d231b79
Robert Schweikert <rjschwei@suse.com>
2021-11-05 13:13:58 -0400

    - Consistent naming style

--- lib/cloudregister/amazonec2.py
+++ lib/cloudregister/amazonec2.py
@@ -25,32 +25,32 @@ def generateRegionSrvArgs():
     zone_req_header = {}
     
     try:
-        tokenResp = requests.put(token_url, headers=token_header)
-        if tokenResp.status_code == 200:
-            zone_req_header = {'X-aws-ec2-metadata-token': tokenResp.text}
+        token_resp = requests.put(token_url, headers=token_header)
+        if token_resp.status_code == 200:
+            zone_req_header = {'X-aws-ec2-metadata-token': token_resp.text}
     except requests.exceptions.RequestException:
         msg = 'Unable to retrieve IMDSv2 token falling back to IMDSv1'
         logging.warning(msg)
     
-    metaDataUrl = 'http://169.254.169.254/latest/meta-data/'
-    zoneInfo = 'placement/availability-zone'
+    metadata_url = 'http://169.254.169.254/latest/meta-data/'
+    zone_info = 'placement/availability-zone'
 
     try:
-        zoneResp = requests.get(
-            metaDataUrl + zoneInfo, headers=zone_req_header
+        zone_resp = requests.get(
+            metadata_url + zone_info, headers=zone_req_header
         )
     except requests.exceptions.RequestException:
         msg = 'Unable to determine instance placement from "%s"'
-        logging.warning(msg % (metaDataUrl + zoneInfo))
+        logging.warning(msg % (metadata_url + zone_info))
         return
 
-    if zoneResp.status_code == 200:
+    if zone_resp.status_code == 200:
         # For local zones the format is geo-loc-regionid-metro-regionidaz
         # For example us-east-1-iah-1a
         # For regions the format is geo-loc-regionidaz
         # For example us-east-1f
         # What we need is geo-loc-regionid, i.e. us-east-1 as the region hint
-        region_data = zoneResp.text.split('-',3)
+        region_data = zone_resp.text.split('-',3)
         region_id_az = region_data[2]
         region_id = ''
         for c in region_id_az:
@@ -59,8 +59,8 @@ def generateRegionSrvArgs():
         region = '-'.join(region_data[:2] + [region_id])
     else:
         logging.warning('Unable to get availability zone metadata')
-        logging.warning('\tReturn code: %d' % zoneResp.status_code)
-        logging.warning('\tMessage: %s' % zoneResp.text)
+        logging.warning('\tReturn code: %d' % zone_resp.status_code)
+        logging.warning('\tMessage: %s' % zone_resp.text)
         return
 
     return 'regionHint=' + region
