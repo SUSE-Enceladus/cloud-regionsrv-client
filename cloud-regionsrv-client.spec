SUSE-Enceladus/cloud-regionsrv-client

commit 531d162a2cfb1ef2b0e1da09a9229ccbb682e810

Changes to support SLE-Micro
+ SLE-Micro has a read-only root file system and handles files in /var
  in a specific manner. These changes modify locations of specific files
  to accomodate the SLE-Micro setup.

cloud-regionsrv-client.spec
@@ -16,7 +16,7 @@
 #
 
 
-%define base_version 10.0.3
+%define base_version 10.0.4
 Name:           cloud-regionsrv-client
 Version:        %{base_version}
 Release:        0

lib/cloudregister/VERSION
@@ -1,1 +1,1 @@
-10.0.3
+10.0.4

lib/cloudregister/registerutils.py
@@ -40,7 +40,8 @@ from cloudregister import smt
 AVAILABLE_SMT_SERVER_DATA_FILE_NAME = 'availableSMTInfo_%s.obj'
 HOSTSFILE_PATH = '/etc/hosts'
 NEW_REGISTRATION_MARKER = 'newregistration'
-REGISTRATION_DATA_DIR = '/var/lib/cloudregister/'
+OLD_REGISTRATION_DATA_DIR = '/var/lib/cloudregister/'
+REGISTRATION_DATA_DIR = '/var/cache/cloudregister/'
 REGISTERED_SMT_SERVER_DATA_FILE_NAME = 'currentSMTInfo.obj'
 RMT_AS_SCC_PROXY_MARKER = 'rmt_is_scc_proxy'
 
@@ -934,7 +935,7 @@ def https_only(config):
 # ----------------------------------------------------------------------------
 def import_smtcert_12(smt):
     """Import the SMT certificate on SLES 12"""
-    key_chain = '/usr/share/pki/trust/anchors'
+    key_chain = '/etc/pki/trust/anchors'
     if not smt.write_cert(key_chain):
         return 0
     if not update_ca_chain(['update-ca-certificates']):
@@ -1257,8 +1258,11 @@ def uses_rmt_as_scc_proxy():
 
     return os.path.isfile(get_state_dir() + RMT_AS_SCC_PROXY_MARKER)
 
+
+# ----------------------------------------------------------------------------
 def get_state_dir():
     return REGISTRATION_DATA_DIR
+
 
 # Private
 # ----------------------------------------------------------------------------

usr/sbin/updatesmtcache
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (c) 2020, SUSE LLC, All rights reserved.
+# Copyright (c) 2022, SUSE LLC, All rights reserved.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -16,8 +16,21 @@
 """Update the cached SMT server objects to set the configured protocol"""
 
 import glob
+import os
 
 import cloudregister.registerutils as utils
+
+if os.path.exists(utils.OLD_REGISTRATION_DATA_DIR):
+    # The case where both new and old location exist cannot happen. This
+    # code runs on package update and as such moves the directory.
+    #
+    # The hard coded path ends in '/' thus producing an empty entry in the
+    # split(). We want to make sure we do not create nested dirs.
+    os.system(
+        'mv %s %s' % (utils.OLD_REGISTRATION_DATA_DIR,
+                os.sep.join(utils.REGISTRATION_DATA_DIR.split(os.sep)[:-2])
+        )
+    )
 
 cached_server_files = glob.glob('%s/*SMT*' % utils.REGISTRATION_DATA_DIR)
 

commit 2baa379a707f8b55b22735d3b612e56cf5892f64

Fixes for ro root
- While the code was updated for the read-only root setup of SLE-Micro the
  spec file was not updated to accomodate the new locations for the cache
  that is needed by cloud-regionsrv-client and the generic config was not
  updated for the cert location of region server certs. This update addresses
  the problem introduced with 531d162a2cfb

cloud-regionsrv-client.spec
@@ -119,8 +119,10 @@ python3 setup.py build
 cp -r etc %{buildroot}
 cp -r usr %{buildroot}
 python3 setup.py install --prefix=%{_prefix}  --root=%{buildroot}
-mkdir -p %{buildroot}/var/lib/regionService/certs
-mkdir -p %{buildroot}/var/lib/cloudregister
+# The location of the regionserver certs
+mkdir -p %{buildroot}/usr/lib/regionService/certs
+# The directory for the cache data
+mkdir -p %{buildroot}/var/cache/cloudregister
 install -d -m 755 %{buildroot}/%{_mandir}/man1
 install -m 644 man/man1/* %{buildroot}/%{_mandir}/man1
 install -m 644 usr/lib/systemd/system/regionsrv-enabler-azure.service %{buildroot}%{_unitdir}
@@ -160,7 +162,7 @@ gzip %{buildroot}/%{_mandir}/man1/*
 %dir %{_usr}/lib/zypp
 %dir %{_usr}/lib/zypp/plugins
 %dir %{_usr}/lib/zypp/plugins/urlresolver
-%dir /var/lib/cloudregister
+%dir /var/cache/cloudregister
 %{_mandir}/man*/*
 %{_sbindir}/containerbuild-regionsrv
 %{_sbindir}/cloudguest-repo-service
@@ -180,8 +182,8 @@ gzip %{buildroot}/%{_mandir}/man1/*
 
 %files generic-config
 %defattr(-,root,root,-)
-%dir /var/lib/regionService
-%dir /var/lib/regionService/certs
+%dir /usr/lib/regionService
+%dir /usr/lib/regionService/certs
 %config %{_sysconfdir}/regionserverclnt.cfg
 
 %files plugin-gce