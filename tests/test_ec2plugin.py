877ed7f6c5488037f45bf89baa5ae67f8529f844
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-04-15 11:46:26 +0100

    Update amazonec2 loop
    When zone_req_header is present we update imds_ips, making the loop of length 1
    
    - Update the test to cover the case where zone_req_header is not present, so
      loop has length 2
    
    - Update amazonec2, if no return the code will iterate in the for loop, no need
      for a 'continue' statement, seems redundant

--- tests/test_ec2plugin.py
+++ tests/test_ec2plugin.py
@@ -69,13 +69,22 @@ def test_request_fail_response_error(
         mock_logging, mock_request_get, mock_request_put
 ):
     """Test unexpected return value"""
-    mock_request_get.return_value = _get_error_response()
+    # make sure loop has two IP addresses
+    mock_request_put.side_effect = [_get_error_response(), _get_error_response()]
+    mock_request_get.side_effect = [_get_error_response(), _get_error_response()]
     result = ec2.generateRegionSrvArgs()
     assert result is None
     assert mock_logging.warning.called
     msg = '\tMessage: Test server failure'
-    mock_logging.warning.assert_called_with(msg)
-
+    assert mock_logging.warning.call_args_list == [
+        call('Falling back to IMDSv1'),
+        call('Unable to get availability zone metadata'),
+        call('\tReturn code: 500'),
+        call('\tMessage: Test server failure'),
+        call('Unable to get availability zone metadata'),
+        call('\tReturn code: 500'),
+        call('\tMessage: Test server failure')
+    ]
 
 # ----------------------------------------------------------------------------
 @patch('amazonec2.requests.put')
