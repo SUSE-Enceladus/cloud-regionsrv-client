27a84eefcd725036d226ff035c3b173a00433b55
Marcus Schäfer <marcus.schaefer@suse.com>
2025-09-04 11:18:35 +0200

    Fix missing sys import
    There is code that calls sys.stderr without having sys
    imported. How can that have ever worked ? Also setuptools
    is a requirement no longer automatically included with
    newer python versions, fetch from pip when testing

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@
 # Lesser General Public License for more details.
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.
+import sys
 
 try:
     import setuptools
